6b15d43e91b27e56f2caca3cbccf002547aeee94
DanielSBrown <danny.brown@viget.com>
2020-06-25 11:05:47 -0400

    Working sample app

--- app/controllers/viewer_tutorials_controller.rb
+++ app/controllers/viewer_tutorials_controller.rb
@@ -8,6 +8,7 @@ class ViewerTutorialsController < ApplicationController
 
   def create
     @pdf = ViewerTutorial.new(pdf_params)
+    @pdf.text_content = pdf_text_scraper(@pdf) if @pdf.valid?
 
     if pdf.save
       redirect_to @pdf, notice: 'Successfully added a PDF.'
@@ -24,6 +25,13 @@ class ViewerTutorialsController < ApplicationController
 
   def pdf_params
     params.require(:viewer_tutorial).permit(:attachment)
+  end
+
+  def pdf_text_scraper(pdf)
+    if pdf.attachment
+      reader = PDF::Reader.new(pdf.attachment.path)
+      reader.pages.map{ |page| page.text }.join
+    end
   end
 
 
--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,7 +1,42 @@
 require 'dragonfly'
+require 'dragonfly/s3_data_store'
+
 Dragonfly.app.configure do
   plugin :imagemagick
 
   secret "..."
+  url_format '/uploads/:job/:name'
+  datastore :file,
+  root_path:   Rails.root.join('public/uploads', Rails.env),
+  server_root: Rails.root.join('public')
 
+  define_url do |app, job, opts|
+    # cropping = PhotoCropping.find_by(signature: job.signature)
+
+    if job.process_steps.empty?
+      # No processing to perform, link to the original
+      app.datastore.url_for(job.uid)
+
+    # elsif cropping
+    #   # A cropping already exists, link to it
+    #   app.datastore.url_for(cropping.uid)
+
+    else
+      # Link to the job
+      app.server.url_for(job)
+    end
+  end
+
+
+end
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
 end
